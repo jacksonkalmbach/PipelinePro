3529ac41276da611b9a274ecb090449fed6863ea
jacksonkalmbach <jacksonrkalmbach@gmail.com>
2023-04-28 11:51:37 -0400

    add nav dropdown

--- client/src/routes/navigation/NavBar.tsx
+++ client/src/routes/navigation/NavBar.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, Link, useLocation } from "react-router-dom";
 import { userSignOut } from "../../store/reducers/user/userAuthSlice";
-import AccoutManagerSelect from "../../components/employee-components/employee-select/EmployeeSelect";
+import EmployeeSelect from "../../components/employee-components/employee-select/EmployeeSelect";
 import "./NavBar.scss";
+
+interface UserData {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  company_name: string;
+  job_title: string;
+  profile_pic: string;
+}
+
+const defaultUserData = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  phone: "",
+  company_name: "",
+  job_title: "",
+  profile_pic: "",
+};
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -11,7 +31,22 @@ const NavBar = () => {
   const location = useLocation();
   const pathname = location.pathname;
 
+  const uid = useSelector((state: any) => state.userAuth.uid);
+  const isSignedIn = useSelector((state: any) => state.userAuth.isSignedIn);
+  const isDemo = useSelector((state: any) => state.userAuth.isDemo);
+  const [userData, setUserData] = useState<UserData>(defaultUserData);
   const [sideNavVisible, setSideNavVisible] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    try {
+      fetch(`http://localhost:5001/employees/${uid}`)
+        .then((res) => res.json())
+        .then((data) => setUserData(data));
+    } catch (error) {
+      console.log("error fetching account info - navbar", error);
+    }
+  }, [uid]);
 
   const toggleSideNav = () => {
     setSideNavVisible(!sideNavVisible);
@@ -25,12 +60,9 @@ const NavBar = () => {
     (state: any) => state.sectionNavigation.value
   );
 
-  const displayName = useSelector((state: any) => state.userAuth.displayName);
-  const firstName = displayName.split(" ")[0];
-  const lastName = displayName.split(" ")[1];
-  const photoURL = useSelector((state: any) => state.userAuth.photoURL);
-  const uid = useSelector((state: any) => state.userAuth.uid);
-  const isSignedIn = useSelector((state: any) => state.userAuth.isSignedIn);
+  const showDropdownOptions = () => {
+    setShowDropdown(!showDropdown);
+  };
 
   const handleLogoClick = () => {
     navigate("/");
@@ -55,18 +87,48 @@ const NavBar = () => {
           <ul className="navbar-links">
             <li className="navbar-link">{currentSection}</li>
             <li className="navbar-link">
-              <div className="demo-account">
-                <AccoutManagerSelect
-                  id={uid}
-                  firstName={firstName}
-                  lastName={lastName}
-                  profilePic={photoURL ? photoURL : firstName[0] + lastName[0]}
-                  nav={true}
-                  title={"Account Manager"}
-                />
-                <Link className="navbar-link" to="/">
-                  <button onClick={handleExitDemo}>Exit Demo</button>
-                </Link>
+              <div className="account">
+                <div onClick={showDropdownOptions}>
+                  <EmployeeSelect
+                    id={uid}
+                    firstName={userData.first_name}
+                    lastName={userData.last_name}
+                    profilePic={
+                      userData.profile_pic
+                        ? userData.profile_pic
+                        : userData.first_name[0] + userData.last_name[0]
+                    }
+                    nav={true}
+                    title={userData.job_title}
+                  />
+                  {showDropdown && (
+                    <div className="nav-dropdown-container">
+                      <div className="nav-dropdown-item">
+                        <Link
+                          className="nav-dropdown-link"
+                          to="/settings/Account"
+                          onClick={showDropdownOptions}
+                        >
+                          Account
+                        </Link>
+                      </div>
+                      <div className="nav-dropdown-item">
+                        <Link
+                          className="nav-dropdown-link"
+                          to="/login"
+                          onClick={showDropdownOptions}
+                        >
+                          Logout
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+                </div>
+                {isDemo && (
+                  <Link className="navbar-link" to="/">
+                    <button onClick={handleExitDemo}>Exit Demo</button>
+                  </Link>
+                )}
               </div>
             </li>
           </ul>
