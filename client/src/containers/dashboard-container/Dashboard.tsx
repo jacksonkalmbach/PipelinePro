0a0cef2e13ec11c21dc776a55d308333e49d16c4
jacksonkalmbach <jacksonrkalmbach@gmail.com>
2023-04-29 13:41:18 -0400

    add fetch to get dashboard events

--- client/src/containers/dashboard-container/Dashboard.tsx
+++ client/src/containers/dashboard-container/Dashboard.tsx
@@ -8,7 +8,12 @@ import DashboardEvent from "../../components/calendar-components/dashboard-event
 
 const Dashboard = () => {
   const [leads, setLeads] = useState([]);
-  const [todaysEvents, setTodaysEvents] = useState([{}]);
+  const [todaysEvents, setTodaysEvents] = useState([]);
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const dd = String(today.getDate()).padStart(2, "0");
+  const todayDate = `${yyyy}-${mm}-${dd}`;
 
   const currentUserId = useSelector((state: any) => state.userAuth.uid);
 
@@ -21,13 +26,13 @@ const Dashboard = () => {
   }, [currentUserId]);
 
   useEffect(() => {
-    fetch("http://localhost:5001/events/")
+    fetch(`http://localhost:5001/events/${todayDate}/${currentUserId}}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setTodaysEvents(data);
       });
-  }, [currentUserId]);
+  }, [currentUserId, todayDate]);
 
   return (
     <div className="dashboard-container">
@@ -51,7 +56,7 @@ const Dashboard = () => {
         <div className="my-calendar">
           <h2>Today's Calendar</h2>
           <div className="today-events-container">
-            {todaysEvents.length > 0 &&
+            {todaysEvents.length > 0 ? (
               todaysEvents.map((event: any) => {
                 const { event_name, event_time } = event;
                 return (
@@ -60,7 +65,10 @@ const Dashboard = () => {
                     eventTime={event_time}
                   />
                 );
-              })}
+              })
+            ) : (
+              <div className="no-events">No events today</div>
+            )}
           </div>
         </div>
       </div>
